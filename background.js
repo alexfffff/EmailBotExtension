1c36c99e56f11b04fd8c43d760d15d05e700afa2
Michelle Pang <michellepang2000@gmail.com>
2022-10-31 16:35:12 -0400

    stuff

--- background.js
+++ background.js
@@ -7,48 +7,11 @@ chrome.runtime.onInstalled.addListener(() => {
   console.log("Default background color set to %cgreen", `color: ${color}`);
 });
 
-// Listens for the "injectScraper" trigger from the background script
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.ping) {
-    sendResponse({ pong: true });
-    return;
-  } else if (request.message === "injectScraper") {
-    console.log("message received");
-  }
-});
-
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   if (
     changeInfo.status === "complete" &&
     /^https\:\/\/mail\.google\.com\//.test(tab.url)
   ) {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      activeTabID = tabs[0].id;
-
-      chrome.tabs.sendMessage(activeTabID, { ping: true }, (response) => {
-        if (response && response.pong) {
-          // Content script is ready
-          chrome.tabs.sendMessage(activeTabID, { message: "injectScraper" });
-        } else {
-          // No listener on the other end
-          chrome.tabs.executeScript(
-            activeTabID,
-            { file: "./foreground.js" },
-            () => {
-              if (chrome.runtime.lastError) {
-                throw Error("Unable to inject script into tab " + activeTabID);
-              }
-
-              // OK, now it's injected and ready
-              chrome.tabs.sendMessage(activeTabID, {
-                message: "injectScraper",
-              });
-            }
-          );
-        }
-      });
-    });
-
     chrome.scripting
       .insertCSS({
         target: { tabId: tabId },
