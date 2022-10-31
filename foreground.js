60c2bb1850904eace29368cc7867b8203a29587f
egretegret <egret@seas.upenn.edu>
2022-10-31 16:36:01 -0400

    Remove unnecessary code

--- foreground.js
+++ foreground.js
@@ -4,7 +4,7 @@ const ce_name = document.createElement("DIV");
 ce_main_container.classList.add("testelement");
 ce_name.id = "ce_name";
 
-ce_name.innerHTML = `Hello NAME`;
+ce_name.innerHTML = `icon`;
 
 ce_main_container.appendChild(ce_name);
 
@@ -14,7 +14,7 @@ const injectIconIntoContainer = icon => {
 
     let iconContainer = document.getElementsByClassName(
         "G-Ni G-aE J-J5-Ji"
-    )[0];
+    )[1];
 
     if (iconContainer !== undefined && iconContainer != null) {
         iconContainer.appendChild(ce_main_container);
