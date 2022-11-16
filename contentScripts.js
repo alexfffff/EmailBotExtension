e43a08197bc32d0f676d29bbb69c3df5d4926c94
alexfffff <adong9@seas.upenn.edu>
2022-11-16 15:50:49 -0500

    finished testDynamoDB

--- contentScripts.js
+++ contentScripts.js
@@ -22,7 +22,22 @@ var trashList = [];
           }
       }
     };
-
+    function testDynamoDB(){
+      let Http = new XMLHttpRequest();
+      const url='https://rbx505a976.execute-api.us-east-1.amazonaws.com/prod/send_data';
+      Http.open("POST", url);
+      Http.setRequestHeader("Content-Type", "application/json");
+      // Http.setRequestHeader("Access-Control-Allow-Origin", "*");
+      // Http.setRequestHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, X-Auth-Token");
+      // Http.setRequestHeader("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, HEAD, OPTIONS");
+      Http.send();
+      Http.onreadystatechange=async (e)=>{
+          if (Http.readyState == 4 && Http.status == 200) {
+            response = JSON.parse(Http.response);
+            console.log(response);
+          }
+      }
+    }
     
 
 
@@ -55,35 +70,37 @@ var trashList = [];
       const { token} = obj;
       authToken = token;
       promisearr = [];
-      //console.log(token);
+      testDynamoDB();
 
       //get email info ${emailId}?format=minimal
       // listMessages("?maxResults=500&includeSpamTrash=true&q=in:trash")
-      
-      promisearr.push(getEmailPromise("/messages?maxResults=500&includeSpamTrash=true&q=in:trash", "GET", null));
-      //promisearr.push(getEmailPromise("/labels", "GET", null));
-      Promise.all(promisearr).then((response) => {
-        // resonse will be an array of responses from the request
-        emailinfoarr = [];
-        response1 = JSON.parse(response[0]);
-        // log the new 
-        //response2 = JSON.parse(response[1]);
-        //console.log(response2);
-        for (i = 0; i < response1.messages.length; i++) {
-          emailId = response1.messages[i].id;
-          emailinfoarr.push(getEmailPromise(`/messages/${emailId}?format=minimal`, "GET", null));
-          // add no mail label, remove trash label
-          emailinfoarr.push(getEmailPromise(`/${emailId}/modify`, "POST", JSON.stringify({"addLabelIds": ["Label_5248497239207726318"]})));
-        }
-        Promise.all(emailinfoarr).then((response) => { 
 
-          for (i = 0; i < response.length; i++) {
-            response[i] = JSON.parse(response[i]);
-            trashList.push(response[i].snippet);
-          }
-          console.log(trashList);
-        }).catch((error) => {console.error(error.message)});
-      }).catch((error) => {console.error(error.message)});
+
+      
+      // promisearr.push(getEmailPromise("/messages?maxResults=500&includeSpamTrash=true&q=in:trash", "GET", null));
+      // //promisearr.push(getEmailPromise("/labels", "GET", null));
+      // Promise.all(promisearr).then((response) => {
+      //   // resonse will be an array of responses from the request
+      //   emailinfoarr = [];
+      //   response1 = JSON.parse(response[0]);
+      //   // log the new 
+      //   //response2 = JSON.parse(response[1]);
+      //   //console.log(response2);
+      //   for (i = 0; i < response1.messages.length; i++) {
+      //     emailId = response1.messages[i].id;
+      //     emailinfoarr.push(getEmailPromise(`/messages/${emailId}?format=minimal`, "GET", null));
+      //     // add no mail label, remove trash label
+      //     emailinfoarr.push(getEmailPromise(`/${emailId}/modify`, "POST", JSON.stringify({"addLabelIds": ["Label_5248497239207726318"]})));
+      //   }
+      //   Promise.all(emailinfoarr).then((response) => { 
+
+      //     for (i = 0; i < response.length; i++) {
+      //       response[i] = JSON.parse(response[i]);
+      //       trashList.push(response[i].snippet);
+      //     }
+      //     console.log(trashList);
+      //   }).catch((error) => {console.error(error.message)});
+      // }).catch((error) => {console.error(error.message)});
 
     });
 })();
