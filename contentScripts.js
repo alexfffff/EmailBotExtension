5e794059dbaeb017a676cad8c66979b3509ec69b
egretegret <egret@seas.upenn.edu>
2022-11-16 17:33:26 -0500

    Minor format adjustments

--- contentScripts.js
+++ contentScripts.js
@@ -39,35 +39,6 @@ var trashList = [];
       // }).catch((error) => {console.error(error.message)});
     });
 
-    // alex + lucy merge end
-    // frontend start
-
-    const ce_main_container = document.createElement("DIV");
-    const ce_name = document.createElement("DIV");
-
-    ce_main_container.classList.add("testelement");
-    ce_name.id = "ce_name";
-
-    ce_name.innerHTML = `icon`;
-
-    ce_main_container.appendChild(ce_name);
-
-    const injectIconIntoContainer = icon => {
-        // Recursively waits for the icon container to load and injects an
-        // icon into it when it does
-
-        let iconContainer = document.getElementsByClassName(
-            "G-Ni G-aE J-J5-Ji"
-        )[1];
-
-        if (iconContainer !== undefined && iconContainer != null) {
-            console.log("hi");
-            iconContainer.appendChild(ce_main_container);
-        } else {
-            setTimeout(() => injectIconIntoContainer(icon), 200);
-        }
-    };
-
     function listMessages(query) {  
       let Http = new XMLHttpRequest();
       const url='https://gmail.googleapis.com/gmail/v1/users/me/messages' + query;
@@ -127,6 +98,35 @@ var trashList = [];
             };
             Http.onerror = () => reject(Http.statusText);
         })
+    };
+
+    // alex + lucy merge end
+    // frontend start
+
+    const ce_main_container = document.createElement("DIV");
+    const ce_name = document.createElement("DIV");
+
+    ce_main_container.classList.add("testelement");
+    ce_name.id = "ce_name";
+
+    ce_name.innerHTML = `icon`;
+
+    ce_main_container.appendChild(ce_name);
+
+    const injectIconIntoContainer = icon => {
+        // Recursively waits for the icon container to load and injects an
+        // icon into it when it does
+
+        let iconContainer = document.getElementsByClassName(
+            "G-Ni G-aE J-J5-Ji"
+        )[1];
+
+        if (iconContainer !== undefined && iconContainer != null) {
+            console.log("hi");
+            iconContainer.appendChild(ce_main_container);
+        } else {
+            setTimeout(() => injectIconIntoContainer(icon), 200);
+        }
     };
   // injectIconIntoContainer(ce_main_container);
 
